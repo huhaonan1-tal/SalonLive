23c71700319f75c3348bd333e4950af29a76b8af
胡浩南 <huhaonan1@tal.com>
2024-12-10 22:11:24 +0800

    add：直播布局，强制横屏，加载webview

--- app/src/main/java/com/example/salonlive/MainActivity.kt
+++ app/src/main/java/com/example/salonlive/MainActivity.kt
@@ -1,20 +1,61 @@
 package com.example.salonlive
 
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var videoWebView: WebView? = null
+    private var courseWebView: WebView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        initView()
+        webViewSetting()
+    }
+
+    private fun initView() {
+        videoWebView = findViewById(R.id.video_web_view);
+        courseWebView = findViewById(R.id.course_web_view);
+    }
+
+    fun webViewSetting() {
+        courseWebViewSetting()
+        videoWebViewSetting()
+    }
+    //直播视频webView设置
+    fun videoWebViewSetting() {
+        // 启用JavaScript支持
+        val settings = videoWebView?.getSettings()
+        settings?.javaScriptEnabled = true
+        // 确保链接在WebView内打开而不是浏览器
+        videoWebView?.setWebViewClient(WebViewClient())
+        // 加载指定的URL
+        videoWebView?.loadUrl("http://class.jiaoyanyun.com/fe2/main.html?stream_name=fe2")
+    }
+
+    //课件webView设置
+    fun courseWebViewSetting() {
+        // 启用JavaScript支持
+        val settings = courseWebView?.getSettings()
+        settings?.javaScriptEnabled = true
+        // 确保链接在WebView内打开而不是浏览器
+        courseWebView?.setWebViewClient(WebViewClient())
+        // 加载指定的URL
+        courseWebView?.loadUrl("https://www.xiaohongshu.com")
+    }
+
+    override fun onBackPressed() {
+        if (videoWebView!!.canGoBack()) {
+            videoWebView!!.goBack()
+        } else {
+            super.onBackPressed()
         }
     }
 }
